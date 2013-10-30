e88c2b70a5bb26c93238fcc7afa5710bc361decc
Mike Comstock <mikecomstock@gmail.com>
2013-10-30 13:25:41 -0700

    rails new GitSpice -d mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GitSpice::Application.config.secret_key_base = '5938b29e253bb35d6ea659e3b00907759d017ba2b8c858ddc0ef40f9fd7b2f6ae2394f53441711f0bdcdb1ab6ab441ab8e64491f41e09d35b6e3487651b01ace'
