7b2109af7dbd3550a8e57d89b09acafce12a280e
Mike Comstock <mikecomstock@gmail.com>
2014-02-11 14:36:05 -0800

    bootstrap styles are better than no styles

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,12 +3,10 @@ class RepositoriesController < ApplicationController
     full_name = "#{params[:login]}/#{params[:repo]}"
     @client = Octokit::Client.new :access_token => current_user.oauth_token
     @repo = @client.repo full_name
-    @tree = @client.tree full_name, @repo.default_branch
-
+    #@tree = @client.tree full_name, @repo.default_branch
     #TODO: sort contents
     @contents = @client.contents full_name, :path => params[:content_path]
-
-    @branches =[]# @client.branches full_name
+    @branches = @client.branches full_name
     #debugger
   end
 end
